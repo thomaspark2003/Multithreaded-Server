9c01169619ed8d154de3709fa82a62bd0922e83b
Thomas Park <thomaspark@uvic.ca>
2025-04-25 19:31:45 -0700

    update main file for infinite objects v0.04

--- octosling.py
+++ octosling.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from sprites import Player
 from sprites import Objects
+from sprites import Camera
 import time
 from debug import debug
 
@@ -9,19 +10,21 @@ class Game:
     def __init__(self):
         pygame.init()
 
-        pygame.display.set_caption('Orb Jump')
+        pygame.display.set_caption('Octo Sling')
         desktop_info = pygame.display.Info()
         self.SCREEN_WIDTH, self.SCREEN_HEIGHT = desktop_info.current_w, desktop_info.current_h
 
         self.clock = pygame.time.Clock()
         self.game_window = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), pygame.FULLSCREEN)
         self.player_image = pygame.image.load('images/player_sprite.png')
+  
         self.rect = self.player_image.get_rect()
 
         self.background_img = pygame.image.load('images/background_doubled.png').convert()
 
-        self.objects = Objects(self.SCREEN_WIDTH, self.SCREEN_HEIGHT, self.game_window)
-        self.char = Player(self.game_window, self.rect, self.SCREEN_WIDTH, self.SCREEN_HEIGHT, self.background_img)
+        self.objects = Objects(self.SCREEN_WIDTH, self.SCREEN_HEIGHT, self.game_window, self.background_img)
+        self.char = Player(self.game_window, self.rect, self.SCREEN_WIDTH, self.SCREEN_HEIGHT, self.objects.grab_rect, self.objects.grab_mask)
+        self.camera = Camera(self.SCREEN_WIDTH, self.SCREEN_HEIGHT, self.char.rect, self.game_window, self.objects, self.char)
         
         self.running = True
 
@@ -38,9 +41,10 @@ class Game:
 
             self.clock.tick(60)
 
-            self.char.move(self.objects.floor_rect, self.objects.left_wall_rect, self.objects.right_wall_rect, self.objects.left_wall_mask)
-            self.objects.update()
-
+            self.char.move(self.objects.floor_rect, self.objects.left_wall_rect, self.objects.left_wall_mask, self.camera)
+            self.objects.update(self.camera, self.char)
+            self.camera.object_offset(self.objects, self.char)
+            
             pygame.display.update()
 
 def main():
